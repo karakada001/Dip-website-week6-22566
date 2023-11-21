c0f926c1173f07bd155173e014805eca2462b751
Khunaiko <pasuaiko21089@gmail.com>
2023-11-21 11:06:52 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <!-- <Compute /> -->
+    <!-- <TwoWay /> -->
+    <getter-setter />
+  </div>
+</template>
+
+<script>
+// import TwoWay from './components/TwoWay.vue';
+// import Compute from './components/Compute.vue';
+import GetterSetter from './components/GetterSetter.vue';
+export default {
+    name:"App",
+    components: {
+      // TwoWay,
+      // Compute,
+      GetterSetter,
+    },
+  }
+</script>
